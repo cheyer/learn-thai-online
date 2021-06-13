b2ef062aa5a7a2a8eea59815f05ba267723a0454
Chris Heyer <cheyer03@gmail.com>
2021-06-13 16:14:36 +0200

    initial commit

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Learn Thai Online'
+const name = siteTitle
 
 export default function Layout({ children, home }) {
   return (
