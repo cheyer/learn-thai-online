64e0ec6159898d79985083d3542e76ba8f46a56b
Chris Heyer <cheyer03@gmail.com>
2021-06-23 23:53:57 +0200

    fix(bot): fix async/await

--- lib/bot/index.ts
+++ lib/bot/index.ts
@@ -3,10 +3,10 @@ import { containsCommand } from "./commands";
 import { isCallback } from "./utils/isCallback";
 import { IUpdate } from "./vendor";
 
-export const processRequest = (update: IUpdate) => {
+export const processRequest = async (update: IUpdate) => {
   if (isCallback(update)) {
-    checkAnswer(update);
+    await checkAnswer(update);
   } else {
-    containsCommand(update);
+    await containsCommand(update);
   }
 };
--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,6 +1,6 @@
 import { processRequest } from "../../lib/bot";
 
-export default (req, res) => {
+export default async (req, res) => {
   processRequest(req.body);
-  res.status(200).json({ text: `Hello Telegram Bot` });
+  return res.status(200).json({ text: `Hello Telegram Bot` });
 };
