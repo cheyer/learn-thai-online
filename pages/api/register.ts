093afb4d5e6b719cb60355f03fe64dbf32c6709f
Chris Heyer <cheyer03@gmail.com>
2021-06-23 23:51:42 +0200

    refactor(register): extrat bot path from domain

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -4,11 +4,11 @@ export default async (req, res) => {
 
   try {
     const fetchResult = await fetch(
-      `https://api.telegram.org/bot${secret}/setWebhook?url=${domain}`
+      `https://api.telegram.org/bot${secret}/setWebhook?url=${domain}/api/bot`
     );
     const data = await fetchResult.json();
-    res.status(200).json({ text: "✅ Webhook successfully set!", data });
+    return res.status(200).json({ text: "✅ Webhook successfully set!", data });
   } catch {
-    res.status(200).json({ text: "❌ Error in setting webhook!" });
+    return res.status(200).json({ text: "❌ Error in setting webhook!" });
   }
 };
