138927730d335ee0bf3e8dc57e2bb160475852e3
Chris Heyer <cheyer03@gmail.com>
2021-07-08 02:19:17 +0200

    feat: add og image

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,9 +19,7 @@ export default function Layout({ children, home }) {
           />
           <meta
             property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            content="https://www.learnthai.online/images/og-image.png"
           />
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
